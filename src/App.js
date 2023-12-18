4678047072bdd56e3a89c4c569c1f51b5ed3c1c6
Allanchristopher <allanchristopher2516@gmail.com>
2023-12-18 18:26:14 +0530

    first responsive

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React,{useEffect} from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Ourfacilities from './components/Ourfacilities';
-import About from './components/About';
-import Rooms from './components/Rooms';
-import Gallery from './components/Gallery';
-import Testimonial from './components/Testimonial';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Ourfacilities from "./components/Ourfacilities";
+import About from "./components/About";
+import Rooms from "./components/Rooms";
+import Gallery from "./components/Gallery";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 function App() {
-  useEffect(() =>{
-    window.scrollTo(0,0);
-  },[]);
-  const handleClick = (button) => {
-  };
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  const handleClick = (button) => {};
   return (
     <div className="App">
-    <Navbar/>
-    <Home/>
-    <Ourfacilities/>
-     <About/>
-     <Rooms/>
-     <Gallery/>
-     <Testimonial/>
-    <Contact/>
-    <Footer/>
+      <Navbar />
+      <Home />
+      <Ourfacilities />
+      <About />
+      <Rooms />
+      <Gallery />
+      <Testimonial />
+      <Contact />
+      <Footer />
     </div>
-
   );
 }
 
 export default App;
+
+//
+// <Rooms/>
+// <Gallery/>
+// <Testimonial/>
+// <Contact/>
+// <Footer/>
